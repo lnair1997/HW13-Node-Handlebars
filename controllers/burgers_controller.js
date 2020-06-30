d2754e64a00513209266b607924bedf77797ece3
lathisha <lathisha.n@gmail.com>
2020-06-30 15:01:39 -0700

    controller setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,40 +1,31 @@
 const express = require("express");
-
 const router = express.Router();
-
-// Import the model (cat.js) to use its database functions.
-const cat = require("../models/cat.js");
+// Import the model to use its database functions.
+const burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     const hbsObject = {
-      cats: data
+      burger: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.json({ id: data.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
-  const condition = "id = " + req.params.id;
-
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
+  burger.update({devoured: req.body.devoured}, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -45,11 +36,10 @@ router.put("/api/cats/:id", function(req, res) {
 });
 
 router.delete("/api/cats/:id", function(req, res) {
-  const condition = "id = " + req.params.id;
+  let condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
