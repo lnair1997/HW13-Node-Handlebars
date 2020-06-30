18574ee1404a32acad013ea94e66bdd33132fa37
lathisha <lathisha.n@gmail.com>
2020-06-30 06:51:21 -0700

    database setup

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const app = express();
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+// Import routes and give the server access to them.
+const routes = require("./controllers/catsController.js");
 
 const PORT = process.env.PORT || 8080;
-
-const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -11,19 +14,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-const routes = require("./controllers/catsController.js");
-
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log(`Server listening on: http://localhost: ${PORT}`);
 });
